fd63d142eb3415de85b7015f5edcbf4816bb6797
Moe-hacker <admin@crack.moe>
2023-11-07 00:36:41 +0800

    xxxx

--- 12cpus/clock.lua
+++ 12cpus/clock.lua
@@ -59,402 +59,422 @@ settings_table = {
 		end_angle=360
 	}, -- ]]
 	{
-		name='time',
-		arg='%M.%S',
-		max=60,
-		bg_colour=0xffffff,
-		bg_alpha=0.2,
-		fg_colour=0xA2D2FF,
-		fg_alpha=0.6,
-		x=160, y=155,
-		radius=80,
-		thickness=4,
-		start_angle=0,
-		end_angle=360
+		name = 'time',
+		arg = '%M.%S',
+		max = 60,
+		bg_colour = 0xffffff,
+		bg_alpha = 0.2,
+		fg_colour = 0xA2D2FF,
+		fg_alpha = 0.6,
+		x = 160,
+		y = 155,
+		radius = 80,
+		thickness = 4,
+		start_angle = 0,
+		end_angle = 360
 	},
 	{
-		name='cpu',
-		arg='cpu1',
-		max=100,
-		bg_colour=0xffffff,
-		bg_alpha=0.2,
-		fg_colour=0xcdb4db,
-		fg_alpha=1,
-		x=160, y=155,
-		radius=89,
-		thickness=4,
-		start_angle=80,
-		end_angle=208
+		name = 'cpu',
+		arg = 'cpu1',
+		max = 100,
+		bg_colour = 0xffffff,
+		bg_alpha = 0.2,
+		fg_colour = 0xffffff,
+		fg_alpha = 1,
+		x = 160,
+		y = 155,
+		radius = 89,
+		thickness = 4,
+		start_angle = 80,
+		end_angle = 208
 	},
 	{
-		name='cpu',
-		arg='cpu2',
-		max=100,
-		bg_colour=0xffffff,
-		bg_alpha=0.2,
-		fg_colour=0xcdb4db,
-		fg_alpha=1,
-		x=160, y=155,
-		radius=93,
-		thickness=4,
-		start_angle=80,
-		end_angle=208
+		name = 'cpu',
+		arg = 'cpu2',
+		max = 100,
+		bg_colour = 0xffffff,
+		bg_alpha = 0.2,
+		fg_colour = 0xffffff,
+		fg_alpha = 1,
+		x = 160,
+		y = 155,
+		radius = 93,
+		thickness = 4,
+		start_angle = 80,
+		end_angle = 208
 	},
 
 	{
-		name='cpu',
-		arg='cpu3',
-		max=100,
-		bg_colour=0xffffff,
-		bg_alpha=0.2,
-		fg_colour=0xcdb4db,
-		fg_alpha=1,
-		x=160, y=155,
-		radius=97,
-		thickness=4,
-		start_angle=80,
-		end_angle=208
-        },
+		name = 'cpu',
+		arg = 'cpu3',
+		max = 100,
+		bg_colour = 0xffffff,
+		bg_alpha = 0.2,
+		fg_colour = 0xffffff,
+		fg_alpha = 1,
+		x = 160,
+		y = 155,
+		radius = 97,
+		thickness = 4,
+		start_angle = 80,
+		end_angle = 208
+	},
 
 	{
-			name='cpu',
-			arg='cpu4',
-			max=100,
-			bg_colour=0xffffff,
-			bg_alpha=0.2,
-			fg_colour=0xcdb4db,
-			fg_alpha=1,
-			x=160, y=155,
-			radius=101,
-			thickness=4,
-			start_angle=80,
-			end_angle=208
-		},
-		{
-			name='cpu',
-			arg='cpu5',
-			max=100,
-			bg_colour=0xffffff,
-			bg_alpha=0.2,
-			fg_colour=0xcdb4db,
-			fg_alpha=1,
-			x=160, y=155,
-			radius=105,
-			thickness=4,
-			start_angle=80,
-			end_angle=208
-	},	{
-		name='cpu',
-		arg='cpu6',
-		max=100,
-		bg_colour=0xffffff,
-		bg_alpha=0.2,
-		fg_colour=0xcdb4db,
-		fg_alpha=1,
-		x=160, y=155,
-		radius=109,
-		thickness=4,
-		start_angle=80,
-		end_angle=208
-},	
-{
-		name='cpu',
-		arg='cpu7',
-		max=100,
-		bg_colour=0xffffff,
-		bg_alpha=0.2,
-		fg_colour=0xcdb4db,
-		fg_alpha=1,
-		x=160, y=155,
-		radius=113,
-		thickness=4,
-		start_angle=80,
-		end_angle=208
-},
-{
-		name='cpu',
-		arg='cpu8',
-		max=100,
-		bg_colour=0xffffff,
-		bg_alpha=0.2,
-		fg_colour=0xcdb4db,
-		fg_alpha=1,
-		x=160, y=155,
-		radius=117,
-		thickness=4,
-		start_angle=80,
-		end_angle=208
-},
-{
-	name='cpu',
-	arg='cpu9',
-	max=100,
-	bg_colour=0xffffff,
-	bg_alpha=0.2,
-	fg_colour=0xcdb4db,
-	fg_alpha=1,
-	x=160, y=155,
-	radius=121,
-	thickness=4,
-	start_angle=80,
-	end_angle=208
-},
-{
-	name='cpu',
-	arg='cpu10',
-	max=100,
-	bg_colour=0xffffff,
-	bg_alpha=0.2,
-	fg_colour=0xcdb4db,
-	fg_alpha=1,
-	x=160, y=155,
-	radius=125,
-	thickness=4,
-	start_angle=80,
-	end_angle=208
-},
-{
-	name='cpu',
-	arg='cpu11',
-	max=100,
-	bg_colour=0xffffff,
-	bg_alpha=0.2,
-	fg_colour=0xcdb4db,
-	fg_alpha=1,
-	x=160, y=155,
-	radius=129,
-	thickness=4,
-	start_angle=80,
-	end_angle=208
-},
-{
-	name='cpu',
-	arg='cpu12',
-	max=100,
-	bg_colour=0xffffff,
-	bg_alpha=0.2,
-	fg_colour=0xcdb4db,
-	fg_alpha=1,
-	x=160, y=155,
-	radius=133,
-	thickness=4,
-	start_angle=80,
-	end_angle=208
-},
-	{
-		name='memperc',  ----物理内存
-		arg='',
-		max=100,
-		bg_colour=0xffffff,
-		bg_alpha=0.2,
-		fg_colour=0xFFc8dd,
-		fg_alpha=0.8,
-		x=160, y=155,
-		radius=113,
-		thickness=40,
-		start_angle=215,
-		end_angle=329
+		name = 'cpu',
+		arg = 'cpu4',
+		max = 100,
+		bg_colour = 0xffffff,
+		bg_alpha = 0.2,
+		fg_colour = 0xffffff,
+		fg_alpha = 1,
+		x = 160,
+		y = 155,
+		radius = 101,
+		thickness = 4,
+		start_angle = 80,
+		end_angle = 208
 	},
 	{
-		name='battery_percent',   ----电池
-		arg='BAT1',
-		max=100,
-		bg_colour=0xffffff,
-		bg_alpha=0.2,
-		fg_colour=0xBDE0FE,
-		fg_alpha=0.8,
-		x=160, y=155,
-		radius=110,
-		thickness=30,
-		start_angle=-25,
-		end_angle=70
+		name = 'cpu',
+		arg = 'cpu5',
+		max = 100,
+		bg_colour = 0xffffff,
+		bg_alpha = 0.2,
+		fg_colour = 0xffffff,
+		fg_alpha = 1,
+		x = 160,
+		y = 155,
+		radius = 105,
+		thickness = 4,
+		start_angle = 80,
+		end_angle = 208
+	}, {
+	name = 'cpu',
+	arg = 'cpu6',
+	max = 100,
+	bg_colour = 0xffffff,
+	bg_alpha = 0.2,
+	fg_colour = 0xffffff,
+	fg_alpha = 1,
+	x = 160,
+	y = 155,
+	radius = 109,
+	thickness = 4,
+	start_angle = 80,
+	end_angle = 208
+},
+	{
+		name = 'cpu',
+		arg = 'cpu7',
+		max = 100,
+		bg_colour = 0xffffff,
+		bg_alpha = 0.2,
+		fg_colour = 0xffffff,
+		fg_alpha = 1,
+		x = 160,
+		y = 155,
+		radius = 113,
+		thickness = 4,
+		start_angle = 80,
+		end_angle = 208
 	},
 	{
-		name='cpu', -- dummy (used for arc)虚拟（用于圆弧）
-		arg='',
-		max=1,
-		bg_colour=0xffffff,
-		bg_alpha=0.8,
-		fg_colour=0xffffff,
-		fg_alpha=0,
-		x=184, y=155,
-		radius=138,
-		thickness=2,
-		start_angle=75,
-		end_angle=105
+		name = 'cpu',
+		arg = 'cpu8',
+		max = 100,
+		bg_colour = 0xffffff,
+		bg_alpha = 0.2,
+		fg_colour = 0xffffff,
+		fg_alpha = 1,
+		x = 160,
+		y = 155,
+		radius = 117,
+		thickness = 4,
+		start_angle = 80,
+		end_angle = 208
 	},
 	{
-		name='cpu', -- dummy (used for arc)虚拟（用于圆弧）
-		arg='',
-		max=1,
-		bg_colour=0xffffff,
-		bg_alpha=0.8,
-		fg_colour=0xffffff,
-		fg_alpha=0,
-		x=266, y=145,
-		radius=335,
-		thickness=2,
-		start_angle=84,
-		end_angle=96
+		name = 'cpu',
+		arg = 'cpu9',
+		max = 100,
+		bg_colour = 0xffffff,
+		bg_alpha = 0.2,
+		fg_colour = 0xffffff,
+		fg_alpha = 1,
+		x = 160,
+		y = 155,
+		radius = 121,
+		thickness = 4,
+		start_angle = 80,
+		end_angle = 208
 	},
 	{
-		name='fs_used_perc',  --外环
-		arg='/',
-		max=100,
-		bg_colour=0xffffff,
-		bg_alpha=0.5,
-		fg_colour=0x98f5e1,
-		fg_alpha=0.8,
-		x=163, y=156,
-		radius=146,
-		thickness=5,
-		start_angle=-147,
-		end_angle=-1.5
+		name = 'cpu',
+		arg = 'cpu10',
+		max = 100,
+		bg_colour = 0xffffff,
+		bg_alpha = 0.2,
+		fg_colour = 0xffffff,
+		fg_alpha = 1,
+		x = 160,
+		y = 155,
+		radius = 125,
+		thickness = 4,
+		start_angle = 80,
+		end_angle = 208
 	},
 	{
-		name='swapperc',  --外环
-		arg='',
-		max=100,
-		bg_colour=0xffffff,
-		bg_alpha=0.5,
-		fg_colour=0xfbf8cc,
-		fg_alpha=0.8,
-		x=163, y=155,
-		radius=145,
-		thickness=5,
-		start_angle=1.5,
-		end_angle=120
+		name = 'cpu',
+		arg = 'cpu11',
+		max = 100,
+		bg_colour = 0xffffff,
+		bg_alpha = 0.2,
+		fg_colour = 0xffffff,
+		fg_alpha = 1,
+		x = 160,
+		y = 155,
+		radius = 129,
+		thickness = 4,
+		start_angle = 80,
+		end_angle = 208
+	},
+	{
+		name = 'cpu',
+		arg = 'cpu12',
+		max = 100,
+		bg_colour = 0xffffff,
+		bg_alpha = 0.2,
+		fg_colour = 0xffffff,
+		fg_alpha = 1,
+		x = 160,
+		y = 155,
+		radius = 133,
+		thickness = 4,
+		start_angle = 80,
+		end_angle = 208
+	},
+	{
+		name = 'memperc', ----物理内存
+		arg = '',
+		max = 100,
+		bg_colour = 0xffffff,
+		bg_alpha = 0.2,
+		fg_colour = 0xFFc8dd,
+		fg_alpha = 0.8,
+		x = 160,
+		y = 155,
+		radius = 113,
+		thickness = 40,
+		start_angle = 215,
+		end_angle = 329
+	},
+	{
+		name = 'battery_percent', ----电池
+		arg = 'BAT1',
+		max = 100,
+		bg_colour = 0xffffff,
+		bg_alpha = 0.2,
+		fg_colour = 0xBDE0FE,
+		fg_alpha = 0.8,
+		x = 160,
+		y = 155,
+		radius = 110,
+		thickness = 30,
+		start_angle = -25,
+		end_angle = 70
+	},
+	{
+		name = 'cpu', -- dummy (used for arc)虚拟（用于圆弧）
+		arg = '',
+		max = 1,
+		bg_colour = 0xffffff,
+		bg_alpha = 0.2,
+		fg_colour = 0xffffff,
+		fg_alpha = 0,
+		x = 184,
+		y = 155,
+		radius = 138,
+		thickness = 2,
+		start_angle = 75,
+		end_angle = 105
+	},
+	{
+		name = 'cpu', -- dummy (used for arc)虚拟（用于圆弧）
+		arg = '',
+		max = 1,
+		bg_colour = 0xffffff,
+		bg_alpha = 0.2,
+		fg_colour = 0xffffff,
+		fg_alpha = 0,
+		x = 266,
+		y = 145,
+		radius = 335,
+		thickness = 2,
+		start_angle = 84,
+		end_angle = 96
+	},
+	{
+		name = 'fs_used_perc', --外环
+		arg = '/',
+		max = 100,
+		bg_colour = 0xffffff,
+		bg_alpha = 0.2,
+		fg_colour = 0x98f5e1,
+		fg_alpha = 0.8,
+		x = 163,
+		y = 156,
+		radius = 146,
+		thickness = 5,
+		start_angle = -147,
+		end_angle = -1.5
+	},
+	{
+		name = 'swapperc', --外环
+		arg = '',
+		max = 100,
+		bg_colour = 0xffffff,
+		bg_alpha = 0.2,
+		fg_colour = 0xfbf8cc,
+		fg_alpha = 0.8,
+		x = 163,
+		y = 155,
+		radius = 145,
+		thickness = 5,
+		start_angle = 1.5,
+		end_angle = 120
 	},
 }
 
 -- Use these settings to define the origin and extent of your clock.
 
-clock_r=75
+clock_r = 75
 
 -- “clock_x”和“clock_y”是时钟中心的坐标，以像素为单位，从conky窗口的左上角开始。
 
-clock_x=160
-clock_y=155
+clock_x = 160
+clock_y = 155
 
 -- 时钟指针的颜色和字母
 
-clock_colour=0xA2D2ff
-clock_alpha=0.8
+clock_colour = 0xA2D2ff
+clock_alpha = 0.8
 
 -- 你想显示秒针吗？
 
-show_seconds=true
+show_seconds = true
 
 require 'cairo'
 
-function rgb_to_r_g_b(colour,alpha)
+function rgb_to_r_g_b(colour, alpha)
 	return ((colour / 0x10000) % 0x100) / 255., ((colour / 0x100) % 0x100) / 255., (colour % 0x100) / 255., alpha
 end
 
-function draw_ring(cr,t,pt)
-	local w,h=conky_window.width,conky_window.height
+function draw_ring(cr, t, pt)
+	local w, h = conky_window.width, conky_window.height
 
-	local xc,yc,ring_r,ring_w,sa,ea=pt['x'],pt['y'],pt['radius'],pt['thickness'],pt['start_angle'],pt['end_angle']
-	local bgc, bga, fgc, fga=pt['bg_colour'], pt['bg_alpha'], pt['fg_colour'], pt['fg_alpha']
+	local xc, yc, ring_r, ring_w, sa, ea = pt['x'], pt['y'], pt['radius'], pt['thickness'], pt['start_angle'],
+		pt['end_angle']
+	local bgc, bga, fgc, fga = pt['bg_colour'], pt['bg_alpha'], pt['fg_colour'], pt['fg_alpha']
 
-	local angle_0=sa*(2*math.pi/360)-math.pi/2
-	local angle_f=ea*(2*math.pi/360)-math.pi/2
-	local t_arc=t*(angle_f-angle_0)
+	local angle_0 = sa * (2 * math.pi / 360) - math.pi / 2
+	local angle_f = ea * (2 * math.pi / 360) - math.pi / 2
+	local t_arc = t * (angle_f - angle_0)
 
 	-- 绘制背景环
 
-	cairo_arc(cr,xc,yc,ring_r,angle_0,angle_f)
-	cairo_set_source_rgba(cr,rgb_to_r_g_b(bgc,bga))
-	cairo_set_line_width(cr,ring_w)
+	cairo_arc(cr, xc, yc, ring_r, angle_0, angle_f)
+	cairo_set_source_rgba(cr, rgb_to_r_g_b(bgc, bga))
+	cairo_set_line_width(cr, ring_w)
 	cairo_stroke(cr)
 
 	-- 牵引指示环
 
-	cairo_arc(cr,xc,yc,ring_r,angle_0,angle_0+t_arc)
-	cairo_set_source_rgba(cr,rgb_to_r_g_b(fgc,fga))
+	cairo_arc(cr, xc, yc, ring_r, angle_0, angle_0 + t_arc)
+	cairo_set_source_rgba(cr, rgb_to_r_g_b(fgc, fga))
 	cairo_stroke(cr)
 end
 
-function draw_clock_hands(cr,xc,yc)
-	local secs,mins,hours,secs_arc,mins_arc,hours_arc
-	local xh,yh,xm,ym,xs,ys
+function draw_clock_hands(cr, xc, yc)
+	local secs, mins, hours, secs_arc, mins_arc, hours_arc
+	local xh, yh, xm, ym, xs, ys
 
-	secs=os.date("%S")
-	mins=os.date("%M")
-	hours=os.date("%I")
+	secs = os.date("%S")
+	mins = os.date("%M")
+	hours = os.date("%I")
 
-	secs_arc=(2*math.pi/60)*secs
-	mins_arc=(2*math.pi/60)*mins+secs_arc/60
-	hours_arc=(2*math.pi/12)*hours+mins_arc/12
+	secs_arc = (2 * math.pi / 60) * secs
+	mins_arc = (2 * math.pi / 60) * mins + secs_arc / 60
+	hours_arc = (2 * math.pi / 12) * hours + mins_arc / 12
 
 	-- 时针抽签
 
-	xh=xc+0.7*clock_r*math.sin(hours_arc)
-	yh=yc-0.7*clock_r*math.cos(hours_arc)
-	cairo_move_to(cr,xc,yc)
-	cairo_line_to(cr,xh,yh)
+	xh = xc + 0.7 * clock_r * math.sin(hours_arc)
+	yh = yc - 0.7 * clock_r * math.cos(hours_arc)
+	cairo_move_to(cr, xc, yc)
+	cairo_line_to(cr, xh, yh)
 
-	cairo_set_line_cap(cr,CAIRO_LINE_CAP_ROUND)
-	cairo_set_line_width(cr,5)
-	cairo_set_source_rgba(cr,rgb_to_r_g_b(clock_colour,clock_alpha))
+	cairo_set_line_cap(cr, CAIRO_LINE_CAP_ROUND)
+	cairo_set_line_width(cr, 5)
+	cairo_set_source_rgba(cr, rgb_to_r_g_b(clock_colour, clock_alpha))
 	cairo_stroke(cr)
 
 	-- 画分针
 
-	xm=xc+clock_r*math.sin(mins_arc)
-	ym=yc-clock_r*math.cos(mins_arc)
-	cairo_move_to(cr,xc,yc)
-	cairo_line_to(cr,xm,ym)
+	xm = xc + clock_r * math.sin(mins_arc)
+	ym = yc - clock_r * math.cos(mins_arc)
+	cairo_move_to(cr, xc, yc)
+	cairo_line_to(cr, xm, ym)
 
-	cairo_set_line_width(cr,3)
+	cairo_set_line_width(cr, 3)
 	cairo_stroke(cr)
 
 	-- 抽出秒针
 
 	if show_seconds then
-		xs=xc+clock_r*math.sin(secs_arc)
-		ys=yc-clock_r*math.cos(secs_arc)
-		cairo_move_to(cr,xc,yc)
-		cairo_line_to(cr,xs,ys)
+		xs = xc + clock_r * math.sin(secs_arc)
+		ys = yc - clock_r * math.cos(secs_arc)
+		cairo_move_to(cr, xc, yc)
+		cairo_line_to(cr, xs, ys)
 
-		cairo_set_line_width(cr,1)
+		cairo_set_line_width(cr, 1)
 		cairo_stroke(cr)
 	end
 end
 
 function conky_clock_rings()
-	local function setup_rings(cr,pt)
-                local str=''
-                local value=0
+	local function setup_rings(cr, pt)
+		local str = ''
+		local value = 0
 
-                str=string.format('${%s %s}',pt['name'],pt['arg'])
-                str=conky_parse(str)
+		str = string.format('${%s %s}', pt['name'], pt['arg'])
+		str = conky_parse(str)
 
-                value=tonumber(str)
-                if value == nil then value = 0 end
-                pct=value/pt['max']
+		value = tonumber(str)
+		if value == nil then value = 0 end
+		pct = value / pt['max']
 
-                draw_ring(cr,pct,pt)
-        end
+		draw_ring(cr, pct, pt)
+	end
 
 
 
 	-- 检查Conky是否已运行至少5秒
 
-	if conky_window==nil then return end
-	local cs=cairo_xlib_surface_create(conky_window.display,conky_window.drawable,conky_window.visual, conky_window.width,conky_window.height)
+	if conky_window == nil then return end
+	local cs = cairo_xlib_surface_create(conky_window.display, conky_window.drawable, conky_window.visual,
+		conky_window.width, conky_window.height)
 
-	local cr=cairo_create(cs)	
+	local cr = cairo_create(cs)
 
-	local updates=conky_parse('${updates}')
-	update_num=tonumber(updates)
+	local updates = conky_parse('${updates}')
+	update_num = tonumber(updates)
 
-	if update_num>5 then
+	if update_num > 5 then
 		for i in pairs(settings_table) do
-			setup_rings(cr,settings_table[i])
+			setup_rings(cr, settings_table[i])
 		end
 	end
 
-	draw_clock_hands(cr,clock_x,clock_y)
+	draw_clock_hands(cr, clock_x, clock_y)
 end
-
